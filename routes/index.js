e5ced690b52482efe9c90a9c961136599d6b01c1
Kenilgajera9801 <84949767+Kenilgajera9801@users.noreply.github.com>
2021-10-12 21:12:35 +0530

    Full Done
    Full Done

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: "Home" });
@@ -13,5 +12,12 @@ router.get('/projects', function(req, res, next) {
 });
 router.get('/contact', function(req, res, next) {
     res.render('contact', { title: "Contact" });
+});
+router.post('/contact', function(req, res, next) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var subject = req.body.subject;
+    var message = req.body.message;
+    res.render('contact', { title: "Contact", message: "Message Sent Successfully" });
 });
 module.exports = router;
